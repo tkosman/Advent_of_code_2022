0790ab37d4afd40c10d70bdd676c2012700ccb4c
Tymoteusz Kosman <tkosman03@gmail>
2022-12-08 15:44:29 +0100

    Day: 6 /25
    Completed day 6.

---
+++ DAY 6/day6_2.py
@@ -0,0 +1,24 @@
+import sys
+
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+
+def ma_powtorzenia(x):
+    for i in range(0, len(x)):
+        score = 0
+        for j in range(0, len(x)):
+            if x[i] == x[j]:
+                score += 1
+        if score > 1:
+            return True
+    return False
+
+for line in lines:
+    szyfr = line.strip()
+    
+for i in range(14, len(szyfr)):
+    do_sprawdzenia = szyfr[i-14:i]
+    if ma_powtorzenia(do_sprawdzenia) == False:
+        print(i)
+        break
+        
